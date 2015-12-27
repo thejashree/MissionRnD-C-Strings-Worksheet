e782f54a38334244783e210a7518c111e545918c
thejashree <thejashree@gmail.com>
2015-12-27 18:45:21 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -12,6 +12,35 @@ NOTES: Don't create new string.
 #include <Stdio.h>
 #include <string.h>
 
+void reverse(char *begin, char *end)
+{
+	char temp;
+	while (begin < end)
+	{
+		temp = *begin;
+		*begin = *end;
+		*end = temp;
+		*begin++;
+		*end--;
+	}
+}
+
 void str_words_in_rev(char *input, int len){
 	
-}
+		char *word_begin = input;
+		char *temp = input; 
+		while (*temp)
+		{
+			temp++;
+			if (*temp == '\0')
+			{
+				reverse(word_begin, temp - 1);
+			}
+			else if (*temp == ' ')
+			{
+				reverse(word_begin, temp - 1);
+				word_begin = temp + 1;
+			}
+		} 
+		reverse(input, temp - 1);
+	}
